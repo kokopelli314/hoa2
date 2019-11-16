ad9bc3486ecbb4bae6135f99763606be8c4ae00f
Nathan Clonts <nathan@servicefusion.com>
2019-11-16 09:31:28 -0700

    basic file uploads

---
+++ src/app.py
@@ -0,0 +1,60 @@
+import os
+from flask import Flask, flash, request, redirect, send_from_directory, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/nathan/hoa2-uploads'
+ALLOWED_EXTENSIONS = { 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif' }
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename: str) -> bool:
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def hello():
+	return 'Hello!!!!'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	"""
+	Upload documents to the system.
+
+	Administrators (or board members?) only.
+	"""
+	if request.method == 'POST':
+		# validations
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+
+		file = request.files['file']
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file', filename=filename))
+
+	# GET requests
+	return '''
+	<!doctype html>
+	<title>Upload New File</title>
+	<h1>Upload New File</h1>
+	<form method=post enctype=multipart/form-data>
+	  <input type=file name=file>
+	  <input type=submit value=Upload>
+	</form>
+	'''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+

c928b64e1c2453be809c8983334c318956aa8000
Nathan Clonts <nathanclonts@gmail.com>
2019-11-16 13:29:40 -0700

    add SQLalchemy database connection

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import os
 from dotenv import load_dotenv
+
 from flask import (
 	Flask, flash, request, redirect, render_template, send_from_directory,
 	url_for,
 )
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from werkzeug.utils import secure_filename
+
 
 load_dotenv()
 
@@ -12,7 +17,13 @@ ALLOWED_EXTENSIONS = { 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif' }
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getenv('DOCUMENT_UPLOAD_FOLDER')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 
 def allowed_file(filename: str) -> bool:
